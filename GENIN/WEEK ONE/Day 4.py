64691b68afc541ebe6fae2f311e901cc9d2e5416
MARK-737 <136316894+MARK-737@users.noreply.github.com>
2023-06-23 16:36:43 -0700

    D4

--- GENIN/WEEK ONE/Day 4.py
+++ GENIN/WEEK ONE/Day 4.py
@@ -1,17 +1,3 @@
-#import random 
-
-#test_seed=int(input("enter seed number"))
-#random.seed(test_seed)
-
-
-#random_side=random.randint(0,1)
-
-#if random_side==0:
-    #print("Heads")
-#else:
-    #print("Tails")
-    
-
 import random
 
 names=input("enter your names separated by comma \n")
@@ -22,3 +8,45 @@ non=len(names_in_list)
 random_size=random.randint(0,non-1)
 who_pays=names_in_list[random_size]
 print(who_pays)
+
+# TREASURE MAP
+
+row1=["", "", ""]
+row2=["", "", ""]
+row3=["", "", ""]
+map=[row1, row2, row3]
+position=input("where you want to put the treasure")
+horizontal=int(position[0])
+vertical=int(position[1])
+selected_row=map[horizontal-1]
+selected_row[vertical-1]="X"
+print(f"{row1}\n{row2}\n{row3}")
+
+
+
+
+
+#ROCK-PAPER-SISSORS GAME
+
+user_choice=int(input("what do you choose? Type o for rock, 1 for paper, or 2 for sissors"))
+computer_choice=random.randint(0,2)
+print(f"computer choose {computer_choice}")
+if user_choice>=3 or user_choice<0:
+    print("you typed an invalid number, Game Over")
+elif user_choice==0 and computer_choice==2:
+    print("you wins")
+elif computer_choice==0 and user_choice==2:
+    print("you lose")
+elif computer_choice>user_choice:
+    print("you lose")
+elif user_choice>computer_choice:
+    ptint("you win")
+elif computer_choice==user_choice:
+    print("ita a draw")    
+ 
+
+
+
+
+
+
